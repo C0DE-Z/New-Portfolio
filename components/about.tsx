cea7c1e91956c83d277d214736426e7d4eff2a9f
C0DE-Z <melonicholas1113@gmail.com>
2024-12-18 12:34:49 -0500

    Update About component for improved styling and structure

--- components/about.tsx
+++ components/about.tsx
@@ -1,8 +1,9 @@
 export default function About() {
     return (
-        <div className="text-center	" >
-            <h2 className="about-title">Hey, I&apos;m Nicholas, a Full-Stack Web Developer with a Passion for Robotics and FPV Drones!</h2>
-            <p className="about-description">I love creating innovative web solutions and exploring the exciting world of robotics and first-person view (FPV) drone technology. Let&apos;s build something amazing together!</p>
+        <div className="text-center	text-[0.8rem] sm:text-[2rem]" >
+            <h1>About Me</h1>
+            <h2 className="about-title text-[0.8rem] sm:text-[1rem]  ">Hey, I&apos;m Nicholas, a Full-Stack Web Developer with a Passion for Robotics and FPV Drones!</h2>
+            <p className="about-description text-[0.8rem] sm:text-[1rem]">I love creating innovative web solutions and exploring the exciting world of robotics and first-person view (FPV) drone technology. Let&apos;s build something amazing together!</p>
         </div>
     )
 }
